a65e19c6d358c22fbdeefb6d7110d95ce582ef77
Zhihao Zhou <zhouzhihao90@gmail.com>
2019-12-09 15:40:24 -0500

    Update train.py

--- train.py
+++ train.py
@@ -215,7 +215,8 @@ def validate(epoch, encoder, decoder, cross_entropy_loss, data_loader, word_dict
 def avg_num_unique_n_grams(all_hypotheses, n=2):
     print(len(all_hypotheses))
     for hypo in zip(all_hypotheses):
-        print(hypo)
+        print(len(hypo))
+        # print(hypo)
         break
     raise Exception()
 
